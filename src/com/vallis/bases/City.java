6024bc1273595e59ac844ff4a68027a57f69a3e0
vallix <vallis726@gmail.com>
2019-01-22 17:39:54 +0800

    1.0

---
+++ src/com/vallis/bases/City.java
@@ -0,0 +1,40 @@
+package com.vallis.bases;
+
+public class City {
+    private String name;
+    private String englishName;
+    private String abbreviateName;
+ private String coordinate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
+
+    public void setEnglishName(String englishName) {
+        this.englishName = englishName;
+    }
+
+    public String getAbbreviateName() {
+        return abbreviateName;
+    }
+
+    public void setAbbreviateName(String abbreviateName) {
+        this.abbreviateName = abbreviateName;
+    }
+
+    public String getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(String coordinate) {
+        this.coordinate = coordinate;
+    }
+}
